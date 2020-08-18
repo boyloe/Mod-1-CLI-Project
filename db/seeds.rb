819c3c6c8599ebd004a3b907e63d2d6d70396e76
boyloe <boyloe@gmail.com>
2020-08-18 15:24:01 -0600

    Update API to handle multiple variables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,18 @@ require 'pry'
 # User.destroy_all
 # Recipe.destroy_all
 # UserRecipe.destroy_all
+protein = 'chicken'
+flavor = 'lemon'
+seasoning = nil
+cuisine_type = 'fried'
 
-
-response = RestClient.get("https://api.edamam.com/search?q=beef&app_id=321076cb&app_key=6dd56cb4f4320dffe50f4b2fcaf529bd")
+#response = RestClient.get("https://api.edamam.com/search?q=beef&app_id=321076cb&app_key=6dd56cb4f4320dffe50f4b2fcaf529bd&from=0&to=5")
+response = RestClient.get("recipepuppy.com/api/?i=#{protein},#{flavor},#{seasoning}")
 
 data = JSON.parse(response)
 #use enumerables to map through data to find what you want to get
 #in the map, you can call the .create method and save to database
 #Character.create
 
-# https://api.edamam.com/search?q=chicken&app_id=${321076cb}&app_key=${6dd56cb4f4320dffe50f4b2fcaf529bdYOUR_APP_KEY}
+
 binding.pry
