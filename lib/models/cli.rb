4c6d2db7009b9dfb7ca1616af6301adb27db9c5f
Bmcalkins <b.raders1013@gmail.com>
2020-08-19 09:27:15 -0600

    favorites

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -22,4 +22,44 @@ class Cli
     end
 end 
 
-    def Menu 
+    def menu 
+        prompt = TTY::Prompt.new
+        menu_options = ["create new recipe", "find a recipe", "favorites", "exit"]
+            menu_prompt = prompt.select("menu", menu_options)
+            case menu_selection
+            when "create new recipe"
+              create_recipe
+            
+            when "find a recipe" 
+              recipe_finder 
+
+            when "favorites"
+              favorites_list
+
+            when "exit"
+              system "exit"
+            end
+        end
+
+        def favorites 
+            favs_list = []
+            fav_input = @user.favorites
+            fav_input.each do |fav|
+            favs_list << fav.recipe.name
+            end
+            puts favs_list 
+                prompt = TTY::Prompt.new
+                favs_options = ["return to main menu", "delete"]
+                response = prompt.select("Favorites", favs_options)
+                case response
+                when "return to main menu"
+                  menu
+                when "delete"
+                  puts "Which favorite would you like to delete?"
+                  this_one = gets.strip
+                  found_fav = Recipe.find_by(name: this_one).id
+                  Favorite.find_by(recipe_id: found_fav, user_id: @user.id).destroy
+                  # system "clear"
+                  # main_menu
+                end
+    
