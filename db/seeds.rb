301871b4a4c3d70eec41fbbc698634a1d39cbab4
boyloe <boyloe@gmail.com>
2020-08-19 14:28:47 -0600

    Removed explicit Recipe.create from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,12 +16,9 @@ UserRecipe.destroy_all
 bryan = User.create(username: 'Bryan')
 bradley = User.create(username: 'Bradley')
 
-teryaki_beef = Recipe.create(name: "Beef Teryaki Recipe", ingredients: "beef, mirin, chicken broth, soy, sauce, teriyaki sauce, sugar, cornstarch, water")
-grilled_italian_chicken = Recipe.create(name: "Grilled Italian Chicken Breasts", ingredients: "garlic, bread crumbs, chicken, butter")
-
-UserRecipe.create(user: bradley , recipe: teryaki_beef)
-UserRecipe.create(user: bryan , recipe: teryaki_beef)
-UserRecipe.create(user: bryan , recipe: grilled_italian_chicken)
+UserRecipe.create(user: bradley , recipe: Recipe.first)
+UserRecipe.create(user: bryan , recipe: Recipe.first)
+UserRecipe.create(user: bryan , recipe: Recipe.second)
 
 def get_recipes
     proteins = ['chicken', 'beef','pork','fish']
